a85cebcaa63ed8136e537121d12a16b079b49072
SamMyers455 <70865991+SamMyers455@users.noreply.github.com>
2020-10-29 19:39:39 -0500

    Update for.sh

--- for.sh
+++ for.sh
@@ -1,4 +1,4 @@
-#The shell scripting guide from Project 2 did not have us making a permanentscript to 
+#The shell scripting guide from Project 2 did not have us making a permanent script to 
 #perform the function of a for loop. Since we did not create one for Project 2,
 #I decided to make this code block to include it in the repository and describe
 #it's purpose. It's purpose is to run a loop while a condition is true, until the
